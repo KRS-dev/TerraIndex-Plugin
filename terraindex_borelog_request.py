c94412c2948a5750954196511137c2ba15e8f7dc
K.R.Schuurman <57346737+KRS-dev@users.noreply.github.com>
2022-10-24 14:16:59 +0200

    crossection maptool designed

--- terraindex_borelog_request.py
+++ terraindex_borelog_request.py
@@ -87,10 +87,7 @@ class BorelogRequest:
         self.plugin = plugin
         self.iface = plugin.iface
 
-        # self.username = kwargs.get('username', plugin.username)
-        # self.password = kwargs.get('password', plugin.password)
-        # self.ln = kwargs.get('licensenumber', plugin.ln)
-        # self.ac = kwargs.get('applicationcode', plugin.ac)
+        self.url = 'https://web.terraindex.com/DataWSExternals/ITWBoreprofileService_V1_0.svc?singleWsdl'
 
         self.borelogParameters = {
             'PageNumber': kwargs.get('PageNumber', '1'),
@@ -103,6 +100,16 @@ class BorelogRequest:
             'Layout' : None
         }
 
+        self.checkLayoutTemplate()
+
+        self.boreholes = []
+        self.xml = None
+
+    def addBorehole(self, BoreHoleID: int, ProjectID: int):
+        self.boreholes.append(
+            {'BoreHoleID': str(BoreHoleID), 'ProjectID': str(ProjectID)})
+
+    def checkLayoutTemplate(self):
         # Load in the layout file
         if self.plugin.layoutsDict == {} or not isinstance(self.plugin.layoutsDict, dict):        
             with open(os.path.join(self.plugin.plugin_dir, 'data', r'depots 4 blad.txt'), encoding='utf8') as f:
@@ -117,13 +124,6 @@ class BorelogRequest:
             self.borelogParameters['Layout'] = self.plugin.getLayout(layoutID)
             self.borelogParameters['LayoutName'] = layout['TemplateName']
 
-        
-        self.boreholes = []
-        self.xml = None
-
-    def addBorehole(self, BoreHoleID: int, ProjectID: int):
-        self.boreholes.append(
-            {'BoreHoleID': str(BoreHoleID), 'ProjectID': str(ProjectID)})
 
     def setXMLparameters(self):
 
@@ -172,7 +172,6 @@ class BorelogRequest:
         if self.xml is None:
             self.setXMLparameters()
 
-        url = 'https://web.terraindex.com/DataWSExternals/ITWBoreprofileService_V1_0.svc?singleWsdl'
 
         token = self.plugin.token
 
@@ -180,9 +179,60 @@ class BorelogRequest:
             'Authorization' : 'Bearer {}'.format(token) 
         }
 
-        response = requests.post(url=url, data=self.xml, headers=headers)
+        response = requests.post(url=self.url, data=self.xml, headers=headers)
 
         return response
+
+class CrossSectionRequest(BorelogRequest):
+
+    def __init__(self, plugin):
+        super().__init__(plugin, **kwargs)
+
+        # Overwrite the standard input arguments
+        self.borelogParameters['DrawMode'] = kwargs.get('DrawMode', 'Multipage')
+        self.borelogParameters['OutputType'] = kwargs.get('OutputType', 'PDF')
+
+
+    def setXMLparameters(self):
+
+        # Open the xml request blueprint
+        xmlfile = os.path.join(self.plugin.plugin_dir, 'data',
+                               'Borelog_Request_SOAP.xml')
+
+        with open(xmlfile, 'r') as f:
+            xml_base = f.read()
+
+        root = ET.fromstring(xml_base)
+
+        
+        authorisationParameters ={
+            'Username' : self.plugin.username,
+            'ApplicationCode' : self.plugin.applicationcode,
+            'Licensenumber': self.plugin.licensenumber
+        }
+
+        for key, val in authorisationParameters.items():
+            elem = root.find('.//c:{}'.format(key), ns)
+            elem.text = str(val)
+
+        for key, val in self.borelogParameters.items():
+            elem = root.find('.//b:{}'.format(key), ns)
+            elem.text = val
+
+        assert len(self.boreholes) > 0, 'no boreholes selected'
+
+        # append new element for each borehole
+        elem_boreholes = root.find('.//b:Boreholes', ns)
+        for borehole_dict in self.boreholes:
+            b = ET.SubElement(elem_boreholes, "{" + ns['b'] + "}Borehole")
+            BoreHoleID = ET.SubElement(b, "{" + ns['b'] + "}BoreHoleID")
+            ProjectID = ET.SubElement(b, "{" + ns['b'] + "}ProjectID")
+            BoreHoleID.text = borehole_dict['BoreHoleID']
+            ProjectID.text = borehole_dict['ProjectID']
+
+        self.xml = ET.tostring(root)
+
+
 
 
 class BoreholeDataRequest:
@@ -243,21 +293,3 @@ class BoreholeDataRequest:
             return response, data
     
 
-
-
-
-
-
-class BoreHoleImage:
-    """Simple class to keep the image together with the boreholeID and projectID."""
-
-    def __init__(self,  BoreHoleID, ProjectID, bytes64=None):
-        self.id = BoreHoleID    
-        self.projectID = ProjectID
-        self.bytes = base64.b64decode(bytes64)
-
-        # with BytesIO(self.bytes) as stream:
-        #     self.image = Image.open(stream).convert("RGBA")
-
-    # def show(self):
-    #     self.image.show()
---
+++ terraindex_crosssection_tool.py
@@ -0,0 +1,168 @@
+from qgis.gui import QgsMapTool
+from qgis.core import  QgsPointXY, QgsProject, QgsPointXY, QgsRectangle, QgsWkbTypes
+
+from qgis.PyQt.QtGui import QTextDocument, Qt
+from qgis.PyQt.QtCore import QSizeF
+
+from collections import OrderedDict
+
+import numpy as np
+
+
+
+
+class TICrosssectionTool(QgsMapTool):
+    """Pointtool class, which overwrites the normal cursor during use of the plugin"""
+    def __init__(self, iface, plugin):
+        super(QgsMapTool, self).__init__(iface.mapCanvas())
+
+        self.iface = iface
+        self.plugin = plugin
+
+        self.isEmittingPoint = False
+
+        self.rubberband = QgsRubberBand(self.canvas(), geometryType=QgsWkbTypes.LineString)
+
+        self.distances = {}
+
+
+
+    def canvasPressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            if not self.isEmittingPoint:
+                self.startPoint = self.toMapCoordinates(event.pos())
+                self.endPoint = self.startPoint
+                self.isEmittingPoint = True 
+            
+            else:
+                self.endPoint = self.toMapCoordinates(event.pos())
+
+                if self.startPoint.compare(self.endPoint):
+                    self.unsetSelectedFeatures()
+                    self.rubberband.hide()
+                    self.isEmittingPoint = False
+
+
+                else:
+                    self.unsetSelectedFeatures()
+                    bbRect_map = self.bbRect(10)
+                    
+
+                    if bbRect_map is not None:
+                        layer = self.plugin.TILayer
+                        bbRect = self.canvas().mapSettings().mapToLayerCoordinates(layer, r)
+                        features = layer.getFeatures(bbRect)
+
+                        startpt_layer = self.canvas().mapSettings().mapToLayerCoordinates(layer, self.startPoint)
+                        
+                        proj_vec = self.getProjVec()
+
+                        for f in features:
+                            vec = f.geometry().asPoint() - startpt_layer
+                            self.distances[f.id()] = self.proj_to_line(vec, proj_vec)
+                            
+
+                        self.setSelectedFeatures([f for f in features])
+
+        
+        elif event.button() == Qt.RightButton:
+
+            self.isEmittingPoint = False
+            self.unsetSelectedFeatures()
+            self.rubberband.hide()
+
+            
+
+
+    def canvasMoveEvent(self, event):
+        if not self.isEmittingPoint:
+            return
+
+        self.endPoint = self.toMapCoordinates(event.pos())
+        self.showLine(self.startPoint, self.endPoint)
+
+    def canvasReleaseEvent(self, event):
+        pass          
+
+    def showLine(self, startPoint, endPoint):
+        self.rubberband.reset(QgsWkbTypes.LineString)
+        
+        if startPoint.x() == endPoint.x() or startPoint.y() == endPoint.y():
+            return
+        point1 = QgsPointXY(startPoint.x(), startPoint.y())
+        point2 = QgsPointXY(endPoint.x(), endPoint.y())
+
+        self.rubberband.addPoint(point1, False)
+        self.rubberband.addPoint(point2, True) # true to update canvas  
+        self.rubberband.show()
+
+    def bbRect(self, thickness):
+        '''Creates a rectangle with thickness around the line segment
+
+        Args:
+            thickness (float): Thickness within the given coordinate systems xy-plane 
+
+        Returns:
+            QgsRectangle: Rectangle around the Line
+        '''        
+
+        if self.startPoint is None or self.endPoint is None:
+            return None
+        elif self.startPoint.compare(self.endPoint):
+            return None
+        else:
+            vec_norm = self.getProjVec()
+            vec_orth = np.array([vec_norm[1], -1*vec_norm[0]])
+
+            a = thickness/2
+
+            point1 = QgsPointXY( startPoint.x() + a*vec_orth[0], startPoint.y() + a*vec_orth[1])
+            point2 = QgsPointXY( endPoint.x() - a*vec_orth[0], endPoint.y() - a*vec_orth[1])
+            return QgsRectangle(point1, point2)
+    
+    def getProjVec(self):
+        if self.startPoint is None or self.endPoint is None:
+            return None
+        else:
+            vec = self.endPoint - self.startPoint
+            return vec.normalized()
+
+    def proj_to_line(self, vec1, vec2):
+        '''Projects vector 1 onto the line of vector 2
+
+        Args:
+            vec1 (QgsVector): vector 1
+            vec2 (QgsVector): vector 2 that spans a projection line
+
+        Returns:
+            _type_: _description_
+        '''        
+        d = vec1 * vec2/(vec2 * vec2)
+        return d
+    
+
+    def setSelectedFeatures(self, features):
+        self.plugin.TILayer.selectByIds([f.id() for f in features])
+        
+    def unsetSelectedFeatures(self):
+        self.plugin.TILayer.removeSelection()
+
+    def getSelectedFeature(self):
+        return self.plugin.TILayer.selectedFeatures()
+
+
+    def deactivate(self):
+
+        self.rubberband.reset()
+        
+        QgsMapTool.deactivate(self)
+
+
+    def isZoomTool(self):
+        return False
+
+    def isTransient(self):
+        return False
+
+    def isEditTool(self):
+        return False
